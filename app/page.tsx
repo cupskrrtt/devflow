723191cb49445bead67c0742495371a62322db87
cupskrrtt <khanalfarizzi@hotmail.com>
2024-11-23 04:20:43 +0700

    defaulting to basic nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
+"use client";
 import { Button } from "@/components/ui/button";
+import { userSignIn, userSignUp } from "@/lib/data/auth";
+import { useActionState } from "react";
 
-export default async function Home() {
+export default function Home() {
+	const [state, formAction, pending] = useActionState(userSignUp, {
+		message: "",
+		type: "",
+	});
 	return (
-		<section>
+		<section className="flex flex-col gap-2">
 			<p>KNTL</p>
 			<Button asChild>
 				<a href="/dashboard">To dashboard</a>
 			</Button>
+			<form action={formAction}>
+				<Button type="submit">Get Cookie</Button>
+			</form>
+
+			<form action={userSignIn}>
+				<Button type="submit">Test Cookie</Button>
+			</form>
 		</section>
 	);
 }
--- lib/data/auth.ts
+++ lib/data/auth.ts
@@ -1,21 +1,51 @@
 "use server";
 
-export async function userSignUp(data: FormData) {
-	const yal = await fetch("http://localhost:3000/api/auth/signup", {
-		method: "POST",
-		body: data,
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+interface FormStatus {
+	type: string;
+	message: string;
+	error?: any;
+}
+
+export async function userSignUp(
+	prev: FormStatus,
+	data: FormData,
+): Promise<FormStatus> {
+	const cookieStore = await cookies();
+	const yal = await fetch("http://localhost:3001/", {
+		method: "GET",
 	});
 
-	console.log(await yal.json());
+	const cookieData = (await yal.json()).data;
+
+	console.log(cookieData);
+
+	cookieStore.set("access_token", cookieData, {
+		httpOnly: true,
+		secure: true,
+		sameSite: "none",
+	});
+
+	return {
+		type: "SUCCESS",
+		message: "Cookie setup",
+	};
 }
 
 export async function userSignIn(data: FormData) {
-	const yal = await fetch("http://localhost:3000/api/auth/signin", {
-		method: "POST",
-		body: data,
-	});
+	const cookieStore = await cookies();
 
-	console.log(await yal.json());
+	const act = cookieStore.get("access_token")?.value;
+	console.log(act);
+
+	await fetch("http://localhost:3001/", {
+		method: "POST",
+		headers: {
+			Authorization: `Bearer ${act}`,
+		},
+	});
 }
 
 export async function cookieCheck() {
